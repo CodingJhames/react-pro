692152cbd8f6bea4b529c5dc407baab24c6a0de5
codingjhames <jhames.mejia@unipamplona.edu.co>
2022-09-18 18:37:08 -0500

    greensock animatio added once reach maximum value

--- bases/src/bases/CounterEffect.tsx
+++ bases/src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
 
 const maximum_count = 10;
 export const CounterEffect = (  ) => {
@@ -17,12 +18,17 @@ export const CounterEffect = (  ) => {
 
       if ( counter < 10 ) return;
       console.log('%cSe llegó al valor máximo','color:red; background-color: white;');
+      
+      gsap.to('h2',{y:-10, duration: 0.2, ease:'ease.out' }).then( () => {
+          gsap.to('h2', { y:0, duration: 1, ease:'bounce.out' } )
+      } )
 
     }, [counter])
     
   return (
     <>
-        <h1>CounterEffect : { counter }</h1>
+        <h1>CounterEffect : </h1>
+        <h2>{ counter }</h2>
         <button
         onClick={ handleClick }>+ 1</button>
 
